f333da729ac3645e6b55defaf0c5efa493d1a3dd
Wonssun <heidencena1@gmail.com>
2020-11-30 00:40:31 +0900

    initial commit

---
+++ src/main/java/com/qnaboard/myqnaboard/MyQnaboardApplication.java
@@ -0,0 +1,13 @@
+package com.qnaboard.myqnaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQnaboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyQnaboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qnaboard/myqnaboard/MyQnaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnaboard.myqnaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQnaboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
