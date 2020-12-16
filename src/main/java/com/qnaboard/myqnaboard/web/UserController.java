bd24b96b0710de5277832a39989be9b55cc3440a
Wonssun <heidencena1@gmail.com>
2020-12-16 11:26:52 +0900

    새로운 html 적용 및 url 리펙토링, 개인정보 수정 기능 구현

--- src/main/java/com/qnaboard/myqnaboard/web/UserController.java
+++ src/main/java/com/qnaboard/myqnaboard/web/UserController.java
@@ -1,28 +1,49 @@
 package com.qnaboard.myqnaboard.web;
 
+import com.qnaboard.myqnaboard.domain.User;
+import com.qnaboard.myqnaboard.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-    private List<User> users = new ArrayList<>();
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/create")
-    public String create(User user) {
-        System.out.println("user : " + user);
-        users.add(user);
-        return "redirect:/list";
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
     }
 
-    @GetMapping("/list")
+    @PostMapping("")
+    public String create(User user) {
+        System.out.println("user : " + user);
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", users);
-        return "list";
+        model.addAttribute("users", userRepository.findAll());
+        return "/user/list";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model) {
+        model.addAttribute("user", userRepository.findById(id).get());
+        return "/user/updateForm";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, User newUser) {
+        User user = userRepository.findById(id).get();
+        user.update(newUser);
+        userRepository.save(user);
+        return "redirect:/users";
     }
 
 }
