a124c8440425fe43c3f7404b2614c1f999a3bf48
Wonssun <heidencena1@gmail.com>
2020-12-19 11:10:15 +0900

    질문하기 기능 구현

---
+++ src/main/java/com/qnaboard/myqnaboard/web/QuestionController.java
@@ -0,0 +1,43 @@
+package com.qnaboard.myqnaboard.web;
+
+import com.qnaboard.myqnaboard.domain.Question;
+import com.qnaboard.myqnaboard.domain.QuestionRepository;
+import com.qnaboard.myqnaboard.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @GetMapping("/form")
+    public String form(HttpSession session) {
+        if (!HttpSessionUtils.isLoginUser(session)) {
+            return "users/loginForm";
+        }
+        return "qna/form";
+    }
+
+    @PostMapping("")
+    public String create(String title, String contents, HttpSession session) {
+        if (!HttpSessionUtils.isLoginUser(session)) {
+            return "users/loginForm";
+        }
+
+        User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+
+        Question newQuestion = new Question(sessionedUser.getUserId(), title, contents);
+        questionRepository.save(newQuestion);
+
+        return "redirect:/";
+    }
+
+}
