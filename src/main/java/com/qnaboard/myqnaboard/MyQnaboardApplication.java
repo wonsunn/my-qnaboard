a2832ebc44bdfe737f02293007b62cf5a0c7c767
Wonssun <heidencena1@gmail.com>
2020-12-27 01:18:56 +0900

    Swagger 라이브러리 적용

--- src/main/java/com/qnaboard/myqnaboard/MyQnaboardApplication.java
+++ src/main/java/com/qnaboard/myqnaboard/MyQnaboardApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -18,5 +23,4 @@ public class MyQnaboardApplication {
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
     }
-
 }
